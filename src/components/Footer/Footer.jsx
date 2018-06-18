162c1a9973416adbf79d1f4386e9b978334f3a6f
NatoliChris <natolichris@gmail.com>
2018-06-18 22:11:03 +1000

    Add template UI sidebar / top menu

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,50 @@
+import React from "react";
+import PropTypes from "prop-types";
+import FontAwesome from 'react-fontawesome'
+// @material-ui/core components
+import withStyles from "@material-ui/core/styles/withStyles";
+import ListItem from "@material-ui/core/ListItem";
+import List from "@material-ui/core/List";
+// core components
+import footerStyle from "assets/jss/components/footerStyle";
+
+function Footer({ ...props }) {
+  const { classes } = props;
+  return (
+    <footer className={classes.footer}>
+      <div className={classes.container}>
+        <div className={classes.left}>
+          <List className={classes.list}>
+            <ListItem className={classes.inlineBlock}>
+              <a href="/" className={classes.block}>
+                Home
+              </a>
+            </ListItem>
+            <ListItem className={classes.inlineBlock}>
+              <a href="/help" className={classes.block}>
+                  Help
+              </a>
+            </ListItem>
+          </List>
+        </div>
+        <p className={classes.right}>
+          <span>
+            Open source on &nbsp;
+            <a href="https://github.com/sigp/E2E" className={classes.a}>
+              <FontAwesome
+                name="github"
+                size="2x"
+              />
+            </a>
+          </span>
+        </p>
+      </div>
+    </footer>
+  );
+}
+
+Footer.propTypes = {
+  classes: PropTypes.object.isRequired
+};
+
+export default withStyles(footerStyle)(Footer);
---
+++ src/routes/index.jsx
@@ -0,0 +1,5 @@
+import EtoE from "layouts/EtoE/EtoE.jsx"
+
+const indexRoutes = [{ path: "/", component: EtoE}]
+
+export default indexRoutes
