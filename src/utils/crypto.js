681aa07367c9c20f2ba4796034f805003eb75770
Age <Age@AgeManning.com>
2018-07-25 22:01:56 +1000

    Remove cryptographic functions and import ecies-parity

--- src/utils/crypto.js
+++ src/utils/crypto.js
@@ -3,20 +3,19 @@
 * Ethereum uses ECDSA keys for signing transactions. Any Ethereum address is a 160 bits of a Keccak256 hash of the EDSCA public key.
 * this means that encryption cannot be done with the address alone. We will find the public key from past transactions.
 *
-* The encryption scheme used is the Elliptic Curve Integrated Encryption Scheme (ECIES). We will import these from bitcore libraries.
+* The encryption scheme used is the Elliptic Curve Integrated Encryption Scheme (ECIES). 
+* 
+* As parity has implemented their own version of ECIES, I've built a separate
+* module (ecies-parity) which mimics that implementation. We simply import it in this project. 
+*
 */
 
-// RLP encode encrypted messages
-const var rlp = require('rlp');
-// Load Crypto Modules
-// This uses ECDH to encrypt/decrypt, not specifically IES, but satisfactory.
-const var eccrypto = require('eccrypto');
 const var secp256k1 = require('secp256k1');
 const var ethJS = require('ethereumjs-util');
 const ethJS.crypto = require('crypto');
 const ethJS.scrypt = require('scryptsy');
 
-export privateFromFile = function(JSONinput, password) {
+exports.privateFromJSON = function(JSONinput, password) {
 	var json = (typeof JSONinput === 'object') ? JSONinput : JSON.parse(input.toLowerCase());
 	if (json.version !== 3) {
 		throw new Error('Not a V3 wallet')
@@ -46,57 +45,12 @@ export privateFromFile = function(JSONinput, password) {
   return result;
 }
 
-// Encoding - RLP
-// We want to encode before passing through blockchain
-// We build a buffer array and RLP encode it
-let encodeEncryptedJSON = function(_encryptedJSON){
-  var encoded = [];
-  encoded.push(_encryptedJSON['iv']);
-  encoded.push(_encryptedJSON['ephemPublicKey']);
-  encoded.push(_encryptedJSON['ciphertext']);
-  encoded.push(_encryptedJSON['mac']);
-  return rlp.encode(encoded);
-}
-
-// Decode the rlp encoded encrypted object
-let decodeEncryptedJSON = function (_encodedEncryptedJSON){
-  var decodedJSON = {};
-  var decoded = rlp.decode(_encodedEncryptedJSON);
-  decodedJSON.iv = decoded[0];
-  decodedJSON.ephemPublicKey = decoded[1];
-  decodedJSON.ciphertext = decoded[2];
-  decodedJSON.mac = decoded[3];
-  return decodedJSON;
-}
-
-// ECIES Implementation of encryption with RLP encoding
-// returns a promise of RLP encoded encrypted message
-export encryptMessage = function(_publicKey, _message){
-	return new Promise(function(resolve,err){
-			// Encrypt the message
-		  eccrypto.encrypt(_publicKey, Buffer(_message))
-				.then(function(encryptedMessage){
-					resolve(encodeEncryptedJSON(encryptedMessage));
-				})
-				.catch(function(err){
-					console.log(err);
-				});
-
-	});
-}
-
-// Returns a promise of decrypted buffer
-export decryptMessage(_privateKey, _cipherText){
-	var decodedJSON = decodeEncryptedJSON(_cipherText);
-  return eccrypto.decrypt(_privateKey, decodedJSON)
-}
-
 // Key Conversions
-exports privateToPublic = function(_privateKey){
+var privateToPublic = exports.privateToPublic = function(_privateKey){
   return secp256k1.publicKeyCreate(_privateKey, false);
 }
 
-export publicToAddress = function(_pubKey, _sanitize) {
+var publicToAddress = exports.publicToAddress =  function(_pubKey, _sanitize) {
   if (_sanitize && (pubKey.length !== 64)) {
     _pubKey = secp256k1.publicKeyConvert(_pubKey, false);
   }
@@ -105,12 +59,12 @@ export publicToAddress = function(_pubKey, _sanitize) {
   return ethJS.sha3(pubKey).slice(-20)
 }
 
-export privateToAddress = function(_privateKey){
+var privateToAddress = exports.privateToAddress =  function(_privateKey){
   return publicToAddress(privateToPublic(_privateKey));
 }
 
 // Recover public from Hash. (Need original bit so modify ethJS implementation)
-export hashToPublic = function(_hash, _v,_r,_s){
+exports.hashToPublic = function(_hash, _v,_r,_s){
 	var signature = Buffer.concat([ethJS.setLength(_r, 32), ethJS.setLength(_s, 32)], 64);
   var recovery = ethJS.bufferToInt(_v) - 27;
   if (recovery !== 0 && recovery !== 1) {
